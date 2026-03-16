c4db6a57700b9d7a5db7d68f4ccab8471dbe23ea
bunkheangheng <bunkheangheng99@gmail.com>
2026-03-16 11:09:48 +0700

    fixed the robot

--- app/robots.ts
+++ app/robots.ts
@@ -1,16 +1,13 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL || "https://cambo-ea-web.vercel.app";
+const siteUrl = "https://www.camboea.com";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/admin", "/api"],
     },
     sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
-
